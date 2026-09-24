6be98425644b95a8d4042549cd6436bff3bb4208
Satya Panuganti <panugantisatya@gmail.com>
2026-09-24 16:02:41 +0530

    Enable Kafka consumer listener

--- order-service/src/main/java/com/enterprise/ordermanagement/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/enterprise/ordermanagement/order/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.enterprise.ordermanagement.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
 @EnableScheduling
 public class OrderServiceApplication {
 
